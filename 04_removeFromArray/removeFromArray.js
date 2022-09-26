b529b4b45da5bb12c02f48da62c27abe2a8591e2
brandonTsai <brandonya125@gmail.com>
2022-09-26 15:11:39 -0700

    Update files in 04_removeFromArray to pass all the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...theArgs) {
+  let ans = [...arr]
+  for(arg of theArgs) {
+    ans = ans.filter(item => item !== arg)
+  }
+  return ans
 };
 
 // Do not edit below this line
