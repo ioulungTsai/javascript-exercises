5030354d741be7d59fe49ce8a8b7c94cd517cd6e
brandonTsai <brandonya125@gmail.com>
2022-09-26 14:19:48 -0700

    Update repeatString.js & repeatString.spec.js to pass the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let ans = ""
+  if (num < 0) {
+    return 'ERROR'
+  }
+  for(let i =1; i <= num; i++) {
+    ans += str
+  }
+  return ans
 };
 
 // Do not edit below this line
