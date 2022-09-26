9fa0a4d776a7b6000a5468a65c305bf631b1d4ff
brandonTsai <brandonya125@gmail.com>
2022-09-26 15:49:00 -0700

    Update files in 06_leapYears to pass all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if(year % 4 !== 0) {
+    return false
+  }
 
+  if(year % 100 === 0) {
+    return (year/100) % 4 !== 0 ? false : true
+  }
+
+  return true
 };
 
 // Do not edit below this line
